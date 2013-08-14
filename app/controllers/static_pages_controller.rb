986cbb07946c14b33c0941b50546ceaf736cb09a
ericalthatcher <ericalthatcher@gmail.com>
2013-08-14 14:18:39 -0700

    Wrote more copy for About page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,5 +6,10 @@ class StaticPagesController < ApplicationController
   end
 
   def submit
+    # begin
+    #   # need to switch to my params, etc.
+    #   @query_text = params[:zip_code]
+    #   @apitext = Legislator.new(@query_text)
+    # end
   end
 end
