f9499dcf724cf57ca792489c75a20fcd394ef3c4
ericalthatcher <ericalthatcher@gmail.com>
2013-09-10 15:01:20 -0700

    deleted old code and reference to awesome_print

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -1,9 +1,9 @@
 class LegislatorsController < ApplicationController
 
-def index
-  @zip = params[:zip]
-  # Takes zip input, searches new API for legislators in that zipcode and returns results
-  @legislators = Congress.new.legislators_locate(@zip).results
-end
+  def index
+    @zip = params[:zip]
+    # Takes zip input, searches new API for legislators in that zipcode and returns results
+    @legislators = Congress.new.legislators_locate(@zip).results
+  end
 
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,12 @@
+require 'search_congress'
+
 class StaticPagesController < ApplicationController
-  require 'search_congress'
   def about
   end
 
   def home
   end
 
-#  creates new instance of Nancy class, which allows the return of results
-#  for Nancy Pelosi.
   def submit
-    begin
-      @test_legislator = Nancy.new
-    end
   end
 end
