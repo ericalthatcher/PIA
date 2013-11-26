960e16b7e8256935110557566904ea98ca62de13
Erica Thatcher <erica.thatcher@puppetlabs.com>
2013-11-26 15:42:18 -0800

    Restore API functionality and search by zipcode

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,6 +7,11 @@ class StaticPagesController < ApplicationController
   def home
   end
 
+#  creates new instance of Nancy class, which allows the return of results
+#  for Nancy Pelosi.
   def submit
+    begin
+      @test_legislator = Nancy.new
+    end
   end
 end
